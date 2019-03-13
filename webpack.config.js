f3a076c472fa1a53928247de9a28ffb9b4b45ecb
hoangtrongminhduc <hoangtrongminhduc@hotmail.com>
2019-03-13 14:22:26 +0700

    add web config for iis

--- webpack.config.js
+++ webpack.config.js
@@ -4,70 +4,76 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin'); 
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'index.bundle.js'
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './build',
-		hot: true,
-		open: true,
-        port: 3000
-    },
-    module:{
-        rules: [
-		{
-		  test: /\.m?js$/,
-		  exclude: /(node_modules|bower_components)/,
-		  use: {
-			loader: 'babel-loader',
-			options: {
-				  presets: ['@babel/preset-env'],
-				  plugins: ['@babel/plugin-proposal-object-rest-spread']
-				}
-			}
-        },
-       {
-           test: /\.s?css$/,
-           use: [
-                MiniCssExtractPlugin.loader,
-                {
-                    loader: 'css-loader'
-                },
-                {
-                    loader: 'sass-loader'
-                }
-           ],
-       },
-        {
-            test: /\.(png|jpg|gif|svg)$/,
-            use: [{
-                loader: 'file-loader'
-            } ],
-        },
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "index.bundle.js"
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./build",
+    hot: true,
+    open: true,
+    port: 3000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-proposal-object-rest-spread"]
+          }
+        }
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "sass-loader"
+          }
         ]
-    },
-	optimization: {
-		minimizer: [
-			new UglifyJsPlugin(),
-			new OptimizeCSSAssetsPlugin({})
-		]
-	},
-    plugins: [
-		new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin({
-            filename: "[name].css",
-            chunkFilename: "[id].css"
-        }),
-		new HtmlWebpackPlugin({
-		  title: 'My App',
-		  template: path.resolve(__dirname, 'src', 'index.html'),
-		  filename: path.resolve('.', 'build', 'index.html')
-		}
-		)
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: "file-loader"
+          }
+        ]
+      }
     ]
-}
+  },
+  optimization: {
+    minimizer: [new UglifyJsPlugin(), new OptimizeCSSAssetsPlugin({})]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
+    new HtmlWebpackPlugin({
+      title: "My App",
+      template: path.resolve(__dirname, "src", "index.html"),
+      filename: path.resolve(".", "build", "index.html")
+    }),
+    new CopyPlugin([
+      {
+        from: path.resolve(__dirname, "src", "web.config"),
+        to: path.resolve(__dirname, "build"),
+        toType: "dir"
+      }
+    ])
+  ]
+};
