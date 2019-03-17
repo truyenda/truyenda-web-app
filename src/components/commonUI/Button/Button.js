c31aa6510dcfb68bf009ef5257c470d2f4de7e78
hoangtrongminhduc <hoangtrongminhduc@hotmail.com>
2019-03-18 00:04:57 +0700

    improve button

--- src/components/commonUI/Button/Button.js
+++ src/components/commonUI/Button/Button.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
-import styles from './Button.scss';
+import './Button.scss';
 
 class Button extends Component {
     //handle click event by parent callback
   clicked() {
+    if(this.props.onClick)
       this.props.onClick();
-      
   }
   render() {
     return (
       <div>
-            <button onClick={() => this.clicked()} className={'button btn-ok ' + this.props.type}>
-          {this.props.children}
+        <button
+          type={this.props.submit}
+          onClick={() => this.clicked()}
+          className={'button ' + (this.props.type ? this.props.type : 'btn-ok')}
+          disabled={this.props.disabled?this.props.disabled:false}
+        >
+          {this.props.display}
         </button>
       </div>
     );
