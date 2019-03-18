d7c5ffe933a645b73ebfa07dd2f05b1029920388
hoangtrongminhduc <hoangtrongminhduc@hotmail.com>
2019-03-18 14:29:32 +0700

    add example common ui

--- src/screens/App.js
+++ src/screens/App.js
@@ -1,28 +1,82 @@
-import React, { Component } from 'react';
-import logo from '../assets/logo.svg';
-import './App.css';
-import Button from ".././components/commonUI/Button";
-import Progress from ".././components/commonUI/Progress";
-
+import React, { Component } from "react";
+import logo from "../assets/logo.svg";
+import "./App.scss";
+import Button from "../components/commonUI/Button";
+import Progress from "../components/commonUI/Progress";
+import TextInput from "../components/commonUI/TextInput";
+import TextArea from "../components/commonUI/TextArea";
+import CheckBox from '../components/commonUI/CheckBox';
+import SelectBox from '../components/commonUI/SelectBox';
 class App extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      count: 0
+      username: "hello",
+      remember: false,
+      selectbox: 1
     };
   }
-  onClick(){
+  onClick() {
+    console.log(this.state);
     console.log("clicked");
-      
   }
+  setStateForm(key, value) {
+    let fields = this.state;
+    fields[key] = value;
+    this.setState(fields);
+    console.log(this.state);
+  }
+
+  componentWillReceiveProps() {}
   render() {
+    var data = [[0, 'hello'],[1,'konichiwa']]
     return (
       <div className="App">
         <header className="App-header">
-          <Button type='btn-cancel' onClick={this.onClick}>Đăng nhập</Button>
-          <Progress></Progress>
-          <img src={logo} className="App-logo" alt="logo" />
+          <form>
+            <TextInput
+              display="Tài khoản"
+              id="username"
+              onChanged={(key, value) => this.setStateForm(key, value)}
+              value={this.state.username}
+              // type='password'
+              required={true}
+              // disabled={true}
+              // alert="You can not leave empty"
+            />
+            <TextInput
+              display="Mật khẩu"
+              id="password"
+              onChanged={(key, value) => this.setStateForm(key, value)}
+              // value={this.state.username}
+              type="password"
+              required={true}
+              // disabled={true}
+              // alert="You can not leave empty"
+            />
+            <CheckBox
+              id="remember"
+              display="Remember me"
+              checked={this.state.remember}
+              disabled={false}
+              onChanged={(key, value) => this.setStateForm(key, value)}
+            />
+            <SelectBox
+              id="selectbox"
+              display="Thể loại"
+              data={data}
+              selected={this.state.selectbox}
+              // disabled={true}
+              onChanged={(key, value) => this.setStateForm(key, value)}
+            />
+            <Button
+              type="btn-ok"
+              submit="submit"
+              display="Submit"
+              disabled={false}
+              onClick={() => this.onClick()}
+            />
+          </form>
         </header>
       </div>
     );
