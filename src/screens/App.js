d8b854480d8deffe9de9c8f196d2c99409008a39
hoangtrongminhduc <hoangtrongminhduc@hotmail.com>
2019-03-21 00:14:59 +0700

    add simple route

--- src/screens/App.js
+++ src/screens/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 import logo from "../assets/logo.svg";
 import "./App.scss";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import Button from "../components/commonUI/Button";
 import Progress from "../components/commonUI/Progress";
 import TextInput from "../components/commonUI/TextInput";
 import TextArea from "../components/commonUI/TextArea";
 import CheckBox from '../components/commonUI/CheckBox';
 import SelectBox from '../components/commonUI/SelectBox';
+
+import Home from './Home';
+import Login from './Login';
+import SignUp from "./SignUp/SignUp";
 class App extends Component {
   constructor(props) {
     super(props);
@@ -29,56 +35,24 @@ class App extends Component {
 
   componentWillReceiveProps() {}
   render() {
-    var data = [[0, 'hello'],[1,'konichiwa']]
     return (
-      <div className="App">
-        <header className="App-header">
-          <form>
-            <TextInput
-              display="Tài khoản"
-              id="username"
-              onChanged={(key, value) => this.setStateForm(key, value)}
-              value={this.state.username}
-              // type='password'
-              required={true}
-              // disabled={true}
-              // alert="You can not leave empty"
-            />
-            <TextInput
-              display="Mật khẩu"
-              id="password"
-              onChanged={(key, value) => this.setStateForm(key, value)}
-              // value={this.state.username}
-              type="password"
-              required={true}
-              // disabled={true}
-              // alert="You can not leave empty"
-            />
-            <CheckBox
-              id="remember"
-              display="Remember me"
-              checked={this.state.remember}
-              disabled={false}
-              onChanged={(key, value) => this.setStateForm(key, value)}
-            />
-            <SelectBox
-              id="selectbox"
-              display="Thể loại"
-              data={data}
-              selected={this.state.selectbox}
-              // disabled={true}
-              onChanged={(key, value) => this.setStateForm(key, value)}
-            />
-            <Button
-              type="btn-ok"
-              submit="submit"
-              display="Submit"
-              disabled={false}
-              onClick={() => this.onClick()}
-            />
-          </form>
-        </header>
-      </div>
+      <Router>
+        <div>
+          //menu component here
+          <Link to="/">Home</Link>|
+          <Link to="/login">Log In</Link>|
+          <Link to="/signup">Sign Up</Link>
+          <br />
+          // start content area
+          <div className="container">
+            <Switch>
+              <Route path="/" exact={true} component={Home} />
+              <Route path="/login" component={Login} />
+              <Route path="/signup" component={SignUp} />
+            </Switch>
+          </div>
+        </div>
+      </Router>
     );
   }
 }
