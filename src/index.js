af2abfd87f8f6221cc31b989a87bd3e5087a8a78
hoangtrongminhduc <hoangtrongminhduc@hotmail.com>
2019-03-22 00:59:27 +0700

    add redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './screens/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import App from "./screens/App";
+import { createStore, compose, applyMiddleware } from "redux";
+import AppReducer from "./reducers/index.js";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+    AppReducer,
+    composeEnhancer(applyMiddleware(thunk)),
+);
+
+ReactDOM.render(
+  <Provider store = {store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);  
---
+++ src/reducers/ChapterReducer.js
@@ -0,0 +1,11 @@
+var initialState = [];
+
+const ChapterReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return [...state];
+            break;
+    }
+}
+
+export default ChapterReducer;
