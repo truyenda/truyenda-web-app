79b5f8e6dbdb57385f8f5e9dc32d535de358536e
hoangtrongminhduc <hoangtrongminhduc@hotmail.com>
2019-03-22 23:14:49 +0700

    fix call api

--- src/utils/APICaller.js
+++ src/utils/APICaller.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'https://api.truyeda.tk';
+const baseUrl = 'https://api.truyenda.tk';
 
 export default function Caller(endpoint, method = 'GET', body = {}) {
     return axios({
