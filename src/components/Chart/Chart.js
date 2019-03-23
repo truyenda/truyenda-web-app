62d5ce96f8a79600e396bd44a137489e432cadfb
NhatQuangTa <nhatquangta2911@gmail.com>
2019-03-24 01:38:42 +0700

    Demo Home page

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,19 +1,17 @@
 import React, { Component } from "react";
 import styles from "./Chart.scss";
-
+import demo from "../../assets/demo.jpg";
 export default class Chart extends Component {
    render() {
       return (
-         <div className="chart">
             <div className="chart-item">
                <p className="rank">1</p>
-               <img className="img" src="https://via.placeholder.com/90x90"></img>
+               <img className="img" src={demo} />
                <div className="content">
                   <p>Tales of Demons and Gods</p>
                   <p>Mad Snail</p>
                   <p>ONGOING - 344 chapters</p>
                </div>
-            </div>
          </div>
       );
    }
--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Chart from "../../components/Chart/Chart";
 import Browse from "../../components/Browse/Browse";
+import Main from "../../components/Main/Main";
 import styles from "./Home.scss";
 class Home extends Component {
    componentDidMount() {}
@@ -8,23 +9,22 @@ class Home extends Component {
    render() {
       return (
          <div className="home-wrapper">
-            <div className="browse-col">
-               <Browse />
+            <div className="main-content-col">
+               <Main />
             </div>
-            <div className="chart-col">
-               {/* <p className="chart-title">MOST POPULAR</p> */}
-               <Chart />
-               <Chart />
-               <Chart />
-               <Chart />
-               <Chart />
-               <Chart />
-               <Chart />
-               <Chart />
-               <Chart />
-               <Chart />
-               <Chart />
-               <Chart />
+            <div className="side-content-col">
+               <div className="browse-col">
+                  <Browse />  
+               </div>
+               <div className="chart-col">
+                  {/* <p className="chart-title">MOST POPULAR</p> */}
+                  <Chart />
+                  <Chart />
+                  <Chart />
+                  <Chart />
+                  <Chart />
+                  <Chart />
+               </div>
             </div>
          </div>
       );
