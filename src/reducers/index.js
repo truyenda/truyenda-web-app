cae602f6857775888dfb76990cec55839d0e6dcb
hoangtrongminhduc <hoangtrongminhduc@hotmail.com>
2019-04-02 01:01:34 +0700

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,12 @@
 import { combineReducers } from 'redux';
 import ChapterReducer from './ChapterReducer.js';
+import { sessionReducer } from "redux-react-session";
 
+const reducers = {
+    chapter: ChapterReducer,
+    session: sessionReducer
+}
 
-const AppReducer = combineReducers({
-    ChapterReducer
-});
+const AppReducer = combineReducers(reducers);
 
 export default AppReducer;
