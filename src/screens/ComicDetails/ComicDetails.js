33a28f0d7c8f1f4dbee2d4b49a86961061fa7f2a
NhatQuangTa <nhatquangta2911@gmail.com>
2019-04-10 16:19:25 +0700

    Add and minimize the upcoming tasks

--- src/screens/ComicDetails/ComicDetails.js
+++ src/screens/ComicDetails/ComicDetails.js
@@ -1,3 +1,24 @@
-// Split out small steps as much as possible (in-depth thinking)
+/*
 
-//    
+TASK
+
+1. responsive layout (just 2 - 768 and full)
+      1.1. mobile first max-768
+      1.2. full
+
+2. components
+      2.1. cover picture
+      2.2. comic overview
+         2.2.1. comic picture
+         2.2.2. comic intro content
+         2.2.3. (update later) favorite
+      2.3. summary
+      2.4. description
+      2.5. stats
+      2.6. authors
+      2.7. (update later) other facts8.
+      2.8. chapter
+      
+3. Update all in Vietnamese
+
+*/
