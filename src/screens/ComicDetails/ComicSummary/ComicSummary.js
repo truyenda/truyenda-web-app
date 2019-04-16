4eab78325c116f05838b1c2705dc64b8414a6f74
NhatQuangTa <nhatquangta2911@gmail.com>
2019-04-16 14:09:09 +0700

    Auto height (stretch)

--- src/screens/ComicDetails/ComicSummary/ComicSummary.js
+++ src/screens/ComicDetails/ComicSummary/ComicSummary.js
@@ -43,20 +43,6 @@ export default class ComicSummary extends Component {
                      reincarnation and a romantic sub-plot
                   </li>
                </ul>
-               <ul>
-                  <li>
-                     Nie Lie uses his past life's knowledge to survive, save the
-                     world and his loved ones from the future
-                  </li>
-                  <li>
-                     A fantasy read with elements of time traveling, soul
-                     reincarnation and a romantic sub-plot
-                  </li>
-                  <li>
-                     A fantasy read with elements of time traveling, soul
-                     reincarnation and a romantic sub-plot
-                  </li>
-               </ul>
             </div>
             <div className="comic-summary-show-more">
                <p>Show more</p>
