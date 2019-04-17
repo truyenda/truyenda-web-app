3b982cb5c880fea0c6a570a8e2c1fc33a7164b92
hoangtrongminhduc <hoangtrongminhduc@hotmail.com>
2019-04-17 19:15:22 +0700

    improve profile page

--- src/screens/Personal/Profile/Profile.js
+++ src/screens/Personal/Profile/Profile.js
@@ -1,13 +1,360 @@
 import React, { Component } from "react";
 import "./Profile.scss";
-import "../../../components/commonUI/TextInput";
 import TextInput from "../../../components/commonUI/TextInput";
+import TextArea from "../../../components/commonUI/TextArea";
 import SelectBox from "../../../components/commonUI/SelectBox";
 import { useDropzone } from "react-dropzone";
 import Toast from "../../../components/commonUI/Toast";
 import Button from "../../../components/commonUI/Button";
+import FilePicker from "../../../components/commonUI/FilePicker/FilePicker";
+import uploadPhoto from "../../../api/PhotoApi";
+import { sessionService } from "redux-react-session";
+import DefPhoto from "../../../assets/photo.png";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as SessionAction from "../../../actions/SessionActions.js";
+import { withRouter, Redirect } from "react-router-dom";
+import StringUtils from "../../../utils/StringUtils.js";
+class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      componentShow: 1,
+      user: {},
+      name: "",
+      gender: 0,
+      birthday: "",
+      cfPassword: "",
+      newPassword: "",
+      reNewPassword: "",
+      teamName: "",
+      teamDescription: "",
+      teamLogo: "",
+      debug: true
+    };
+  }
+  setComponentShow(com) {
+    this.setState({
+      componentShow: com
+    });
+  }
+  setStateForm(key, value) {
+    this.setState({
+      [key]: value,
+      alert: {}
+    });
+  }
+  componentWillMount() {
+    document.title = "Thông tin tài khoản";
+    sessionService
+      .loadUser()
+      .then(res => this.setState({ user: res }))
+      .catch(err => console.log(err));
+    var mesField = {
+      name: "",
+      gender: 0,
+      birthday: "",
+      cfPassword: "",
+      newPassword: "",
+      reNewPassword: "",
+      teamName: "",
+      teamDescription: "",
+      teamLogo: ""
+    };
+    this.setState({ alert: mesField });
+  }
+  LogoutEvent() {
+    const { logout } = this.props.actions;
+    logout(this.props.history);
+  }
+  ValidInput() {
+    var isValid = true;
+    var alert = {};
+    if (!this.state.name || this.state.name.length === 0) {
+      alert.name = "Tên hiển thị không được để trống";
+      isValid = false;
+    } else {
+      if (this.state.name.length > 32) {
+        alert.name = "Tên hiển thị tối đa 32 ký tự";
+        isValid = false;
+      }
+    }
+    if (!this.state.birthday || this.state.birthday.length === 0) {
+      alert.birthday = "Ngày sinh không được để trống";
+      isValid = false;
+    } else {
+      if (!StringUtils.validateDate(this.state.birthday)) {
+        alert.birthday = "Ngày sinh không đúng định dạng";
+        isValid = false;
+      } else {
+        var age = StringUtils.getAge(this.state.birthday);
+        if (age <= 10 || age >= 100) {
+          alert.birthday = "Tuổi không được chấp nhận";
+          isValid = false;
+        }
+      }
+    }
+    if (!this.state.cfPassword || this.state.cfPassword.length === 0) {
+      alert.cfPassword = "Bạn cần nhập mật khẩu để thực hiện";
+      isValid = false;
+    }
+    if (!this.state.newPassword || this.state.newPassword.length === 0) {
+      alert.newPassword = "Mật khẩu không được để trống";
+      isValid = false;
+    } else {
+      if (
+        this.state.newPassword.length < 6 ||
+        this.state.newPassword.length > 32
+      ) {
+        alert.newPassword = "Mật khẩu phải ít nhất từ 6 đến 32 ký tự";
+        isValid = false;
+      }
+    }
+    if (!this.state.reNewPassword || this.state.reNewPassword.length === 0) {
+      alert.reNewPassword = "Bạn cần nhập xác nhận lại mật khẩu";
+      isValid = false;
+    } else {
+      if (this.state.newPassword !== this.state.reNewPassword) {
+        alert.rePassword = "Mật khẩu mới không khớp";
+        isValid = false;
+      }
+    }
+    this.setState({ alert: alert });
+    return isValid;
+  }
+  UpdateAccount() {
+    if (this.ValidInput()) {
+      console.log(this.state);
+    }
+  }
+  ChangePassword() {
+    if (this.ValidInput()) {
+      console.log(this.state);
+    }
+  }
+  CreateTeam() {}
+  render() {
+    if (!this.state.user.Email) {
+      return <div>Loading</div>;
+    }
+    return (
+      <div>
+        <div className="profile-route-container">
+          <div>
+            <span
+              onClick={e => {
+                this.setComponentShow(1);
+              }}
+              className={this.state.componentShow == 1 ? "active" : ""}
+            >
+              <i className="far fa-address-card" /> Thông tin cá nhân
+            </span>
+            <span
+              onClick={e => {
+                this.setComponentShow(2);
+              }}
+              className={this.state.componentShow == 2 ? "active" : ""}
+            >
+              <i className="fas fa-key" /> Mật khẩu
+            </span>
+            {!this.state.user.Id_NhomDich || this.state.debug ? (
+              <span
+                onClick={e => {
+                  this.setComponentShow(3);
+                }}
+                className={this.state.componentShow == 3 ? "active" : ""}
+              >
+                <i className="fas fa-user-friends" /> Nhóm của bạn
+              </span>
+            ) : (
+              ""
+            )}
+          </div>
+        </div>
+        {this.state.componentShow == 1 ? (
+          <div>
+            {/*TODO: pass function to upload and receive avatar link*/}
+            <ProfileAvatar />
+            <div className="profile-container">
+              <div className="content-space">
+                <h1>Thông tin cá nhân</h1>
+                <div className="profile-form">
+                  <TextInput
+                    display="Email"
+                    value={this.state.user.Email}
+                    disabled={true}
+                  />
+                  <TextInput
+                    display="Tài khoản"
+                    value={this.state.user.Username}
+                    disabled={true}
+                  />
+                  <TextInput
+                    id="name"
+                    display="Tên hiển thị"
+                    onChanged={(key, value) => {
+                      this.setStateForm(key, value);
+                    }}
+                    value={this.state.user.Ten}
+                    alert={this.state.alert.name}
+                  />
+                  <SelectBox
+                    selected={this.state.user.GioiTinh === false ? "0" : "1"}
+                    id="gender"
+                    display="Giới tính"
+                    data={[[1, "Nam"], [0, "Nữ"]]}
+                    onChanged={(key, value) => {
+                      this.setStateForm(key, value);
+                    }}
+                  />
+                  <TextInput
+                    id="birthday"
+                    display="Ngày sinh"
+                    type="date"
+                    onChanged={(key, value) => {
+                      this.setStateForm(key, value);
+                    }}
+                    value={this.state.user.NgaySinh.split("T")[0]}
+                    alert={this.state.alert.birthday}
+                  />
+                  <div className="btn-info-container">
+                    <Button
+                      display="Lưu"
+                      onClick={() => {
+                        this.UpdateAccount();
+                      }}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+        {this.state.componentShow == 2 ? (
+          <div className="profile-container password-con">
+            <div className="content-space">
+              <h1>Mật khẩu tài khoản</h1>
+              <TextInput
+                id="cfPassword"
+                alert={this.state.alert.cfPassword}
+                display="Mật khẩu cũ"
+                onChanged={(key, value) => {
+                  this.setStateForm(key, value);
+                }}
+                type="password"
+              />
+              <TextInput
+                id="newPassword"
+                alert={this.state.alert.newPassword}
+                display="Mật khẩu mới"
+                onChanged={(key, value) => {
+                  this.setStateForm(key, value);
+                }}
+                type="password"
+              />
+              <TextInput
+                id="reNewPassword"
+                alert={this.state.alert.reNewPassword}
+                display="Xác nhận mật khẩu mới"
+                onChanged={(key, value) => {
+                  this.setStateForm(key, value);
+                }}
+                type="password"
+              />
+              <div className="btn-info-container">
+                <Button
+                  display="Đổi mật khẩu"
+                  onClick={() => {
+                    this.ChangePassword();
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+        {this.state.componentShow == 3 && (!this.state.user.Id_NhomDich||
+        this.state.debug) ? (
+          <div className="profile-container password-con">
+            <div className="content-space">
+              <h1>Tạo nhóm dịch của bạn</h1>
+              <TextInput
+                id="teamName"
+                display="Tên nhóm dịch"
+                value={this.state.user.TenNhom}
+                onChanged={(key, value) => {
+                  this.setStateForm(key, value);
+                }}
+              />
+              <TextArea
+                id="teamDescription"
+                display="Mô tả"
+                style="description-team"
+                onChanged={(key, value) => {
+                  this.setStateForm(key, value);
+                }}
+              />
+              {/* <FilePicker
+                multiple={true}
+                mSize={2}
+                onAccept={files => {
+                  //TODO: receive file and save to component state
+                  console.log(files);
+                  files.forEach(file => {
+                    uploadPhoto(file).then(res => console.log(res.data.link));
+                  });
+                }}
+              /> */}
+              <div className="btn-info-container">
+                <Button
+                  display="Tạo"
+                  onClick={() => {
+                    this.CreateTeam();
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+        <div className="btn-logout-container">
+          <Button
+            display="Đăng xuất"
+            type="btn-cancel"
+            style="btn-logout"
+            onClick={() => {
+              this.LogoutEvent();
+            }}
+          />
+        </div>
+      </div>
+    );
+  }
+}
 
-function Profile(props) {
+const mapState = state => ({
+  user: state.session.user,
+  authenticated: state.session.authenticated
+});
+
+const mapDispatch = dispatch => {
+  return {
+    actions: bindActionCreators(SessionAction, dispatch)
+  };
+};
+
+export default withRouter(
+  connect(
+    mapState,
+    mapDispatch
+  )(Profile)
+);
+
+const ProfileAvatar = props => {
   const {
     acceptedFiles,
     rejectedFiles,
@@ -25,60 +372,19 @@ function Profile(props) {
     }
   });
   return (
-    <div>
-      <section className="avatar-container">
-        <div {...getRootProps({ className: "dropzone" })}>
-          <input {...getInputProps()} />
-          <div className="photo-wrapper">
-            <img
-              className="user-avatar"
-              src="https://avatars0.githubusercontent.com/u/31062901?s=88&v=4"
-            />
-            <div className="overlay-avatar">
-              <span className="icon">
-                <i className="fa fa-user" />
-              </span>
-            </div>
-          </div>
-        </div>
-        <aside />
-      </section>
-      <div className="profile-container">
-        <div className="content-space">
-          <h1>Thông tin cá nhân</h1>
-          <div className="profile-form">
-            <TextInput display="Email" value="admin@mail.com" disabled={true} />
-            <TextInput display="Tài khoản" value="admin" disabled={true} />
-            <TextInput display="Tên hiển thị" />
-            <SelectBox
-              id="gender"
-              display="Giới tính"
-              data={[[1, "Nam"], [0, "Nữ"]]}
-              onChanged={(key, value) => {}}
-            />
-            <TextInput display="Ngày sinh" type="date" />
-            <div className="btn-info-container">
-              <Button display="Lưu" />
-            </div>
+    <section className="profile-avatar-container">
+      <div {...getRootProps({ className: "dropzone" })}>
+        <input {...getInputProps()} />
+        <div className="photo-wrapper">
+          <img className="user-avatar" src={props.src ? props.src : DefPhoto} />
+          <div className="overlay-avatar">
+            <span className="icon">
+              <i className="fa fa-user" />
+            </span>
           </div>
         </div>
       </div>
-      <div className="profile-container password-con">
-        <div className="content-space">
-          <h1>Mật khẩu tài khoản</h1>
-          <TextInput display="Mật khẩu cũ" type="password" />
-          <TextInput display="Mật khẩu mới" type="password" />
-          <TextInput display="Xác nhận mật khẩu mới" type="password" />
-          <div className="btn-info-container">
-            <Button display="Đổi mật khẩu" />
-          </div>
-        </div>
-      </div>
-      <div className="btn-logout-container">
-        <Button display="Đăng xuất" type="btn-cancel" style="btn-logout" />
-      </div>
-    </div>
+      <aside />
+    </section>
   );
-}
-
-export default Profile;
+};
