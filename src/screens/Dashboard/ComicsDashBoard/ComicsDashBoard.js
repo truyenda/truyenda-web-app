e81dea7e9671c6e5462874d03491ac4b11cffd7f
NhatQuangTa <nhatquangta2911@gmail.com>
2019-04-24 19:21:30 +0700

    Add Comic Dashboard

--- src/screens/Dashboard/ComicsDashBoard/ComicsDashBoard.js
+++ src/screens/Dashboard/ComicsDashBoard/ComicsDashBoard.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
-
+import ReactTable from "react-table";
+import "react-table/react-table.css";
 export default class ComicsDashBoard extends Component {
    render() {
       return (
          <div className="comics-dashboard-container">
-            <p>Comics Management</p>
+            <p>Danh sách Truyện</p>
          </div>
       );
    }
