5bb9c006300dd57f9384a3dba5f6804229d98a31
NhatQuangTa <nhatquangta2911@gmail.com>
2019-04-24 12:03:57 +0700

    Add show more for author component

--- src/screens/ComicDetails/ComicAuthors/ComicAuthors.js
+++ src/screens/ComicDetails/ComicAuthors/ComicAuthors.js
@@ -6,14 +6,25 @@ export default class ComicAuthors extends Component {
    constructor(props) {
       super(props);
       this.state = {
-         comic: this.props.details
+         comic: this.props.details,
+         isOpen: false
       };
    }
 
+   toggle() {
+      this.setState({
+         isOpen: !this.state.isOpen
+      });
+   }
+
    render() {
-      const { comic } = this.state;
+      const { comic, isOpen } = this.state;
+      const showMoreClassName = isOpen
+         ? "comic-authors-container-open"
+         : "comic-authors-container-close";
+      const innerShowMore = isOpen ? "Show Less" : "Show More";
       return (
-         <div className="comic-authors-container">
+         <div className={showMoreClassName}>
             <h2>Authors</h2>
             <p>{comic.comicAuthors}</p>
             <div className="comic-list-authors">
@@ -25,7 +36,11 @@ export default class ComicAuthors extends Component {
                <Avatar src={comic.comicAuthorAvatar} />
             </div>
             <div className="comic-authors-show-more">
-               <p>Show more</p>
+               <p
+                  onClick={ () => {
+                     this.toggle();
+                  }}
+               >{innerShowMore}</p>
             </div>
          </div>
       );
