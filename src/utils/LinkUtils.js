21758d0593f97a27ba975a54674e12cc67639a6e
NhatQuangTa <nhatquangta2911@gmail.com>
2019-04-28 20:13:44 +0700

    Move id into first

--- src/utils/LinkUtils.js
+++ src/utils/LinkUtils.js
@@ -8,10 +8,18 @@ export const toComicLink = (title, id) => {
   return converter("comics",title, id);
 };
 
+export const toComicDashboardLink = (title, id) => {
+  return converter("dashboard/comics", title, id);
+}
+
 export const toTeamLink = (name, id) => {
     return converter('teams', name, id);
 }
 
 const converter = (prefix, name, id) => {
     return '/'+prefix+'/'+id+'-'+StringUtils.convertToPath(name);
+}
+
+export const getIdBySplitingPath = (url, prefix) => {
+  return url.split(prefix)[1].split("-")[0];
 }
