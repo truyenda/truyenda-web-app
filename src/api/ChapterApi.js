aa4f51ba13dfa9d2576dcca872ecb24ca24e0332
hoangtrongminhduc <hoangtrongminhduc@hotmail.com>
2019-04-29 23:40:48 +0700

    Add chapter dashboard

---
+++ src/api/ChapterApi.js
@@ -0,0 +1,33 @@
+import Caller from "../utils/APICaller.js";
+
+const prefix = "chapters/";
+
+export default {
+  create(chapter) {
+    var data = {
+      IdTruyen: chapter.IdStory,
+      TenChuong: chapter.title,
+      SoThuTu: chapter.priority,
+      LinkAnh: chapter.links
+    };
+    return Caller(prefix, "POST", data);
+  },
+  list(IdStory) {
+    return Caller("stories/" + IdStory + "/chapters", "GET");
+  },
+  get(IdChapter) {
+    return Caller(prefix + IdChapter, "GET");
+  },
+  update(chapter) {
+    var data = {
+      IdTruyen: chapter.IdStory,
+      TenChuong: chapter.title,
+      SoThuTu: chapter.priority,
+      LinkAnh: chapter.links
+    };
+    return Caller(prefix + chapter.Id, "PUT", data);
+  },
+  delete(chapter) {
+    return Caller(prefix + chapter.Id, "DELETE");
+  }
+};
