3b3c2a5f1c07f3a3c59ce7e9c5f92baa86a854a4
NhatQuangTa <nhatquangta2911@gmail.com>
2019-05-01 15:24:55 +0700

    Add dynamic link to reading page

--- src/screens/ReadingPage/ReadingPage.js
+++ src/screens/ReadingPage/ReadingPage.js
@@ -1,45 +1,70 @@
 import React, { Component } from "react";
 import styles from "./ReadingPage.scss";
 import ComicAuthors from "../ComicDetails/ComicAuthors/ComicAuthors";
+import { getIdBySplitingPath } from "../../utils/LinkUtils";
+import ComicApi from "../../api/ComicApi";
+import NotFound from "../Error/NotFound";
 
 export default class ReadingPage extends Component {
    constructor(props) {
       super(props);
       this.state = {
-         comic: this.props.location.state
+         comic: null,
+         isError: false,
+         isError404: false
       };
    }
 
+   componentDidMount() {
+      try {
+         var url = document.location.href;
+         var id = getIdBySplitingPath(url, "comics/read/");
+         if (!isNaN(id)) {
+            ComicApi.get(id)
+               .then(res => {
+                  this.setState({
+                     comic: res.data.Data
+                  });
+                  document.title = res.data.Data.TenTruyen;
+               })
+               .catch(err => {
+                  this.setState({
+                     isError: true
+                  });
+               });
+         }
+      } catch (err) {
+         this.setState({
+            isError404: true
+         });
+      }
+   }
+
    render() {
-      const {comic} = this.state.comic;
+      const { comic, isError, isError404 } = this.state;
+      if (isError404) {
+         return <NotFound />;
+      }
+      if (isError) {
+         return <span>Có lỗi xảy ra trong quá trình kết nối</span>;
+      }
       return (
          <div className="reading-page-container">
-            <div className="reading-page">
-               <img
-                  src={comic.AnhBia}
-                  alt="Page"
-               />
-               <img
-                  src={comic.AnhBia}
-                  alt="Page"
-               />
-               <img
-                  src={comic.AnhBia}
-                  alt="Page"
-               />
-               <img
-                  src={comic.AnhBia}
-                  alt="Page"
-               />
-               <img
-                  src={comic.AnhBia}
-                  alt="Page"
-               />
-               <img
-                  src={comic.AnhBia}
-                  alt="Page"
-               />
-            </div>
+            {comic && (
+               <div className="reading-page">
+                  <img src={comic.AnhBia} alt="Page" />
+                  <img src={comic.AnhBia} alt="Page" />
+                  <img src={comic.AnhBia} alt="Page" />
+                  <img src={comic.AnhBia} alt="Page" />
+                  <img src={comic.AnhBia} alt="Page" />
+                  <img src={comic.AnhBia} alt="Page" />
+               </div>
+            )}
+            {!comic && (
+               <div className="comic-details">
+                  Nothing to show 
+               </div>
+            )}
          </div>
       );
    }
