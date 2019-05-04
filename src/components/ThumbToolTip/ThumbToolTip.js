f58d3d606fa40d66806a6e52eb11cc64e2043065
hoangtrongminhduc <hoangtrongminhduc@hotmail.com>
2019-05-04 23:25:47 +0700

    Add new tooltip

---
+++ src/components/ThumbToolTip/ThumbToolTip.js
@@ -0,0 +1,42 @@
+import "./ThumbToolTip.scss";
+import React, { Component } from "react";
+import ComicApi from "../../api/ComicApi";
+import Photo from "../../components/commonUI/Photo";
+class ThumbToolTip extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null
+    };
+  }
+
+  componentDidMount() {
+    ComicApi.get(this.props.Id)
+      .then(res => {
+        if (res.data.Code && res.data.Code === 200) {
+          this.setState({ data: res.data.Data });
+        } else {
+          this.setState({ data: null });
+        }
+      })
+      .catch(err => {
+        this.setState({ data: null });
+      });
+  }
+  render() {
+    if (!this.state.data || !this.state.data.TenTruyen) {
+      return <div>Lỗi không thể lấy dữ liệu</div>;
+    }
+    return (
+      <div className='tip-wrap'>
+        <Photo className="thumb-tip" src={this.state.data.AnhDaiDien} />
+        <div className='meta-tip'>
+            <span>{this.state.data.TenTruyen}</span>
+            <p>{this.state.data.MoTa}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ThumbToolTip;
