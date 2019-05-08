ff855b51cbc06499e348445929584e44a6b5f91d
NhatQuangTa <nhatquangta2911@gmail.com>
2019-05-08 23:55:26 +0700

    Add bookmark chapter component

---
+++ src/components/commonUI/Bookmark/Bookmark.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Bookmark extends Component {
+   render() {
+    return (
+      <div className="bookmark-icon">
+         <img src="https://freeiconshop.com/wp-content/uploads/edd/bookmark-flat.png"></img>
+      </div>   
+    )
+  }
+}
+   
