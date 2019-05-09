2bf17c0a2dcd822ce09643ba1036a333185e793c
huynhlv <huynhlevan1997@gmail.com>
2019-05-09 10:11:49 +0700

    add total mangas

--- src/components/Manga/Manga.js
+++ src/components/Manga/Manga.js
@@ -4,13 +4,15 @@ import LatestList from './LatestList';
 import Button from '../commonUI/Button';
 import ChapterApi from '../../api/ChapterApi';
 import Toast from '../commonUI/Toast';
+import Progress from '../commonUI/Progress';
 
 
 export default class Manga extends Component {
   constructor(props) {
     super(props);
     this.state = {
-       mangas: []
+       mangas: null,
+       total_mangas: 0
     };
   }
 
@@ -19,7 +21,8 @@ export default class Manga extends Component {
       .then(res => {
         if (res.data.Code && res.data.Code === 200) {
           this.setState({
-            mangas: res.data.Data
+            mangas: res.data.Data.listNewChuong,
+            total_mangas: res.data.Data.Paging.TotalRecord
           });
         } else {
           Toast.notify(res.data.MsgError, "Mã lỗi " + res.data.Code);
@@ -31,29 +34,36 @@ export default class Manga extends Component {
   }
 
   render() {
-    var elements_mangas = this.state.mangas.map((manga, index) => {
-      return  <div key={ manga.Id } className="alternative_cls">
-                <LatestList
-                  id_chuong = "10"
-                  id_truyen = {manga.Id}
-                  image_manga = {manga.AnhDaiDien}
-                  title={ manga.TenTruyen }
-                  // count_new_chapter={ manga.TenChuong }
-                  new_chapter={ manga.TenChuong }
-                  date_update= { manga.NgayTao }
-                />
-              </div>
-
-    });
+    if(this.state.mangas){
+      var elements_mangas = this.state.mangas.map((manga, index) => {
+        return  <div key={ manga.Id_Truyen } className="alternative_cls">
+                  <LatestList
+                    id_chuong = {manga.Id_Chuong}
+                    id_truyen = {manga.Id_Truyen}
+                    image_manga = {manga.AnhDaiDien}
+                    title={ manga.TenTruyen }
+                    // count_new_chapter={ manga.TenChuong }
+                    new_chapter={ manga.TenChuong }
+                    date_update= { manga.NgayTao }
+                  />
+                </div>
+  
+      });
+    }
     return (
       <div className="main-wrappers">
         <h1 className="_3kDZW">Latest Update</h1>
-        <div className="_3_XVY"><h4>521 manga</h4></div>
+        <div className="_3_XVY"><h4>{this.state.total_mangas} mangas</h4></div>
         <div className="_3X8sC">
           <div className="qjYVyl">Manga</div>
           <div className="qjYVym">New chapters</div>
           <div className="qjYVyr">Date updated</div>
         </div>
+        {!this.state.mangas ? (
+            <Progress display="Đang load truyện..." />
+          ) : (
+            ""
+          )}
         { elements_mangas }
       </div>
     )
